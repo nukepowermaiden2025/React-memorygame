ee7a6e805a717570d3bd2f00df0af699ace4c11b
Kourtney Reynolds <Kourtney.Reynolds@gmail.com>
2018-06-13 17:30:34 -0700

    adding files from old remote

--- scoreboard/src/components/Wrapper.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./Wrapper.css";
-
-//using props.children so that the properties can be passed down from parent
-const Wrapper = props => <div className="wrapper">{props.children}</div>;
-
-export default Wrapper;
-
